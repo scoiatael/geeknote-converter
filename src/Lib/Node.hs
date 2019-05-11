1faa7a9e2aea92396b2e0f8edc6b8c4fe09939e9
lukasz.czaplinski <lukasz.czaplinski@tooploox.com>
2019-05-11 19:22:48 +0200

    Write own conversion

--- src/Lib/Node.hs
+++ src/Lib/Node.hs
@@ -1,9 +1,36 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Lib.Node
     ( toEnml
     ) where
 
 import Data.Text(Text)
-import CMark(nodeToHtml, Node)
+import qualified Data.Text as T
+import CMark(Node(..), NodeType(..))
+
+-- TODO: Maybe Reader would suffice?
+import Control.Monad.Trans.State.Strict(evalStateT, StateT)
+import qualified Control.Monad.Trans.State.Strict as State
+import Data.Functor.Identity(Identity, runIdentity)
+
 
 toEnml :: Node -> Text
-toEnml = nodeToHtml []
+toEnml = runIdentity . eval .  convert
+  where
+    eval = flip evalStateT Blank
+
+data ConverterState = Blank | InList
+type Converter = StateT ConverterState Identity
+
+convert :: Node -> Converter Text
+convert (Node _pos DOCUMENT children) = T.concat <$> mapM convert children
+convert (Node _pos PARAGRAPH children) = T.concat <$> ((mapM convert children) >>= wrap)
+  where
+    wrap ch = maybeWrap ch <$> State.get
+    maybeWrap ch Blank = ["<p>"] ++ ch ++ ["</p>\n"]
+    maybeWrap ch InList = ch
+
+convert (Node _pos (TEXT t) children) = T.concat . ([t]++) <$> mapM convert children
+convert (Node _pos (LIST _lsAttr) children) = State.withStateT (const InList) conversion
+  where
+    conversion = T.concat . (["<ul>\n"]++) .  (++ ["</ul>\n"]) <$> mapM convert children
+convert (Node _pos ITEM children) = T.concat . (["<li>"]++) .  (++ ["</li>\n"]) <$> mapM convert children
