8c35b98e217a9f704171be9dfe021c1fcbdd00a4
lukasz.czaplinski <lukasz.czaplinski@tooploox.com>
2019-05-21 20:05:12 +0200

    Simplify specs

--- test/Spec.hs
+++ test/Spec.hs
@@ -4,7 +4,6 @@ import Test.Hspec
 import Text.RawString.QQ
 import qualified Data.Text as T
 import System.Directory(listDirectory)
-import Control.Monad(void)
 
 import Lib
 
@@ -33,11 +32,11 @@ note inner = (T.concat ["<en-note>", inner, "</en-note>"])
 
 main :: IO ()
 main = hspec $ do
-  describe "toENML" $ do
+  describe "toENML" $
     it "returns body wrapped in ENML" $
       toEnml "\n" `shouldBe` emptyDoc
 
-  describe "fromEnml" $ do
+  describe "fromEnml" $
     it "parses empty note" $
       fromEnml emptyDoc `shouldBe` "\n"
 
@@ -70,7 +69,7 @@ main = hspec $ do
       toEnNoteBody "*emphasized*\n" `shouldBe`  "<p><emph>emphasized</emph></p>\n"
 
   describe "fromEnNote" $ do
-    it "converts unordered list" $ do
+    it "converts unordered list" $
       fromEnNote (note xmlList) `shouldBe` mdList
 
     it "converts paragraph" $
@@ -104,13 +103,13 @@ main = hspec $ do
     let examplesDir = "test/examples"
     examples <- runIO $ listDirectory examplesDir
 
-    let testExample = \example -> do
-          let readToText fname = T.pack <$> (runIO $ readFile $ examplesDir ++ "/" ++ example ++ "/" ++ fname)
+    let testExample exampleFolder = do
+          let readToText fname = T.pack <$> (runIO $ readFile $ examplesDir ++ "/" ++ exampleFolder ++ "/" ++ fname)
           md <- readToText "test.md"
           xml <- readToText "test.xml"
 
-          it ("works for " ++ example) $ do
+          it ("works for " ++ exampleFolder) $ do
             toEnml md `shouldBe` xml
             fromEnml xml `shouldBe` md
 
-    void $ mapM testExample examples
+    mapM_ testExample examples
